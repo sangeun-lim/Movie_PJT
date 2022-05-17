26024726625587103b5f3a7b7a8371e0539ea980
sangeun <lse2625@gmail.com>
2022-05-18 00:12:33 +0900

    0517

--- movie_back/movie_back/settings.py
+++ movie_back/movie_back/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m$t(l=$^y6p0$c=+npyql4u^c!&d@xyes80!#+q3po!d91*pru'
+SECRET_KEY = str(os.getenv('TMDB_API_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'movies',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
