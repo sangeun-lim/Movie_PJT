531af2f9ccba5716514b6b5bc31f7e333bd889cd
seniing <zmmmm111@gmail.com>
2022-05-23 18:10:24 +0900

    0523

---
+++ movie_front/src/views/AdminView.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="goAdmin">Amin</button>
+</template>
+
+<script>
+export default {
+  name: 'AdminView',
+  methods: {
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- movie_front/src/views/HomeView.vue
+++ movie_front/src/views/HomeView.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Movie</h1>
     <!-- Search Bar -->
-
     <!-- Movie Recommend -->
     <div>
       <!-- card 5개 (영화 포스터 / 제목 / 평점 / 개봉날짜 / 줄거리(커서)) -->
@@ -14,6 +13,9 @@
 <script>
   import { mapActions, mapGetters } from 'vuex'
   import RandomMovie from '@/components/movies/RandomMovie.vue'
+
+  import axios from 'axios'
+
 
   export default {
     name: 'HomeView',
